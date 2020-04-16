1fde52b35c31b7644ebba280d3632c0daf618fec
WajahatAliAbid <WajahatAliAbid@users.noreply.github.com>
2020-04-16 07:34:15 +0500

    refactored code

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
+'use-strict'
+
 const sslCertificate = require('get-ssl-certificate');
 const { validateCertToDomain } = require('ssl-validator');
 
+const ArgumentParser = require('argparse').ArgumentParser;
 
-let site_domain = "example.com"
-sslCertificate.get(site_domain).then(async function (certificate) {
-    console.log(JSON.stringify(certificate));
-    let data = await validateCertToDomain(cert=certificate.pemEncoded, domain=site_domain , options=null)
-    console.log(JSON.stringify(data))
+var parser = new ArgumentParser({
+    version: '0.0.1',
+    addHelp: true,
+    description: 'Domain ssl certificate validator'
 });
+parser.addArgument(
+    ['-d', '--domain'],
+    {
+        help: 'Domain to validate for ssl certificate',
+        required: true,
+    }
+);
+let args = parser.parseArgs();
+let site_domain = args.domain;
+(async () => {
+    try {
+        console.info(`Getting certificate for ${site_domain}`);
+        let certificate = await sslCertificate.get(site_domain);
+        console.info("Validating certificate");
+        let data = await validateCertToDomain(cert = certificate.pemEncoded, domain = site_domain, options = null);
+        console.log(`Certificate valid! Issuer: ${data.certInfo.issuer.organization}`);
+    } catch (error) {
+        console.error(error);
+    }
+})();
